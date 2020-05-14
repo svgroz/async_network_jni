a126844112911369d7bf8a23b17cc4363903d910
Simon Grozovsky <svgroz@yandex.ru>
2020-05-14 21:48:06 +0300

    init commit

---
+++ async_network/async_network.cpp
@@ -0,0 +1,8 @@
+﻿#include <string>
+#include <iostream>
+#include <boost/asio.hpp>
+
+
+int main() {
+    return 0;
+}
---
+++ async_network/async_network.h
@@ -0,0 +1,10 @@
+﻿// async_network.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+
+// TODO: Reference additional headers your program requires here.
+
